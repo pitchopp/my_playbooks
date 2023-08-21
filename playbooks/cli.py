781f5fff6027467fcc3118369772dc22c20a6279
Amine SGHIR <mp6099@engie.com>
2023-08-21 16:33:20 +0200

    add full reset command

--- playbooks/cli.py
+++ playbooks/cli.py
@@ -5,18 +5,61 @@ from pathlib import Path
 import colorama as c
 from simple_term_menu import TerminalMenu
 
+
 @click.group()
 def vd():
     """vps deployer"""
 
 
+def _list_apps():
+    apps = []
+    for app in os.scandir(Path(__file__).parent / 'apps'):
+        if app.name.endswith('.yml'):
+            apps.append(app.name[:-4])
+    return apps
+
+
+def _deploy_all():    
+    # deploy all apps
+    click.echo(c.Fore.BLUE + "Deploying all apps..." + c.Fore.RESET)
+    click.echo()
+    apps = _list_apps()
+    
+    for app in apps:
+        # click.echo(c.Fore.BLUE + "Deploying " + c.Style.BRIGHT + app + c.Style.RESET_ALL + c.Fore.BLUE + '...' + c.Fore.RESET)
+        cmd = ["vd", "deploy", app]
+        click.echo(c.Style.DIM + '  > ' + ' '.join(cmd) + c.Style.RESET_ALL)
+        rc = subprocess.call(cmd)
+        if rc != 0:
+            exit(rc)
+        click.echo()
+    click.echo()
+    return rc
+
+
 @vd.command('deploy')
 @click.argument('app', required=False)
 @click.option('--ssh-key', '-k', help='SSH key to use for deployment')
-def deploy(app : str, ssh_key : str):
+@click.option('--all', '-a', help='Deploy all apps', is_flag=True, default=False)
+@click.option('--init', '-i', help='Initilize vps server', is_flag=True, default=False)
+def deploy(app : str, ssh_key : str, all : bool, init : bool):
     """Deploy an app."""
     click.echo()
-    if not app:
+    if all and app:
+        click.echo(c.Fore.RED + 'Cannot use both --all and app name.' + c.Fore.RESET)
+        return
+    if init:
+        # initilize vps server
+        cmd = ["vd", "init"]
+        click.echo(c.Style.DIM + '  > ' + ' '.join(cmd) + c.Style.RESET_ALL)
+        rc = subprocess.call(cmd)
+        if rc != 0:
+            exit(rc)
+        click.echo()
+    
+    if all:
+        exit(_deploy_all())
+    elif not app:
         apps = _list_apps()
         terminal_menu = TerminalMenu(
             apps,
@@ -43,13 +86,6 @@ def deploy(app : str, ssh_key : str):
     exit(rc)
 
 
-def _list_apps():
-    apps = []
-    for app in os.scandir(Path(__file__).parent / 'apps'):
-        if app.name.endswith('.yml'):
-            apps.append(app.name[:-4])
-    return apps
-
 @vd.command('apps')
 def list_apps():
     """List available apps."""
@@ -58,9 +94,10 @@ def list_apps():
     click.echo(c.Style.BRIGHT + 'Available apps:' + c.Style.RESET_ALL)
     click.echo()
     for app in _list_apps():
-            click.echo(c.Fore.CYAN + "- " + app + c.Fore.RESET)
+        click.echo(c.Fore.CYAN + "- " + app + c.Fore.RESET)
     
     click.echo()
+
 
 @vd.command('init')
 def init():
